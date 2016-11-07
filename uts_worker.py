73f8a6f626b91c575debe9d4eaae499ed2a4b973
Qiang Zhou <nju04zq@gmail.com>
2016-11-06 19:29:35 -0800

    add sample input file

--- uts_worker.py
+++ uts_worker.py
@@ -3,7 +3,8 @@
 
 from UtSession import UtSession
 
-session = UtSession("ssh_config.json", "test_script.json", "notify.json")
+json_input = ["ssh_config.json", "test_script.json", "notify.json"]
+session = UtSession(*json_input)
 session.prepare()
 session.go()
 session.send_notify()
